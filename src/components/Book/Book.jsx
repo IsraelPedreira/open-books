74370f631855c9d5853d1a99ca6ce12d233d2442
Israel <israelped10@gmail.com>
2023-11-28 18:51:14 -0300

    Add Book component and grid layout

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,9 @@
+import s from './Book.module.css';
+
+export default function Book() {
+    return(
+        <>
+            <h1>Teste</h1>
+        </>
+    )
+}
---
+++ src/services/axios/axios.js
@@ -0,0 +1,7 @@
+const axios = require('axios')
+
+const instance = axios.create({
+    baseURL: 'https://gutendex.com/',
+  });
+
+module.exports = instance
