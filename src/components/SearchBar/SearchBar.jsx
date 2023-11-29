f81be96d898ad0e1d047ad08dc1c3f5a8695a3f3
Marcio dos Santos Junior <marciojunior.gm@gmail.com>
2023-11-29 14:37:36 -0300

    alteracoes no searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
-
-
 import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import s from './SearchBar.module.css';
 
 function SearchBar({ onSearch }) {
   const [query, setQuery] = useState('');
@@ -13,15 +13,29 @@ function SearchBar({ onSearch }) {
     onSearch(query);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      onSearch(query);
+    }
+  };
+
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Digite sua busca..."
-        value={query}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleSearch}>Buscar</button>
+    <div className={s.searchBarContainer}>
+      <label htmlFor="searchInput">Digite sua busca</label>
+      <div className={s.inputContainer}>
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Dom Casmurro"
+          value={query}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleSearch}>
+          <FaSearch /> 
+        </button>
+      </div>
     </div>
   );
 }
